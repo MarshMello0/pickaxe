abd8dd5f750ac7d41d611c400a4dc7c7a19037a9
Marsh <33008390+MarshMello0@users.noreply.github.com>
2026-09-05 13:15:01 +0100

    Switching to game scene

--- main-menu/MainMenu.cs
+++ main-menu/MainMenu.cs
@@ -1,15 +1,9 @@
 using Godot;
-using System;
 
 public partial class MainMenu : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public void LoadGame()
 	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		GetTree().ChangeSceneToFile("res://game/game.tscn");
 	}
 }
