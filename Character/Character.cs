4bd0ff478e46f8da782c34cb4f094d48b5a0f16a
Marsh <33008390+MarshMello0@users.noreply.github.com>
2026-09-05 15:00:26 +0100

    Moving the target depending on the mouse button pressed

--- Character/Character.cs
+++ Character/Character.cs
@@ -25,4 +25,42 @@ public partial class Character : MeshInstance3D
 		var pos = _viewport.GetMousePosition();
 		return _camera.ProjectPosition(pos, 0);
 	}
+
+	public override void _Input(InputEvent @event)
+	{
+		base._Input(@event);
+
+		if (@event is InputEventMouseButton mouseEvent)
+		{
+			switch (mouseEvent.ButtonIndex)
+			{
+				case MouseButton.Left:
+					if (mouseEvent.Pressed)
+					{
+						_leftArmMarker.GlobalPosition = GetMouseWorldPosition();
+						_leftArmMarker.Reparent(_mouseMarker);
+						_rightArmMarker.Reparent(this);
+					}
+					else if (mouseEvent.IsReleased())
+					{
+						_leftArmMarker.Reparent(this);
+						_rightArmMarker.Reparent(this);
+					}
+					break;
+				case MouseButton.Right:
+					if (mouseEvent.Pressed)
+					{
+						_rightArmMarker.GlobalPosition = GetMouseWorldPosition();
+						_leftArmMarker.Reparent(this);
+						_rightArmMarker.Reparent(_mouseMarker);
+					}
+					else if (mouseEvent.IsReleased())
+					{
+						_leftArmMarker.Reparent(this);
+						_rightArmMarker.Reparent(this);
+					}
+					break;
+			}
+		}
+	}
 }
