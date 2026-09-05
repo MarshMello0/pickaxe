be76c6a5094e31a6f116bb5f5ad1de38704c78c2
Marsh <33008390+MarshMello0@users.noreply.github.com>
2026-09-05 16:38:07 +0100

    Got the arm staying put when clicking

--- Character/Character.cs
+++ Character/Character.cs
@@ -1,27 +1,40 @@
 using Godot;
 
-public partial class Character : MeshInstance3D
+public partial class Character : RigidBody3D
 {
+	[Export] private PinJoint3D _pinJoint;
+
 	[ExportGroup("Targets")]
 	[Export] private Marker3D _mouseMarker;
-	[Export] private Marker3D _rightArmMarker;
-	[Export] private Marker3D _leftArmMarker;
 
 	private Viewport _viewport;
 	private Camera3D _camera;
+	private bool _canStick;
+	private bool _isStuck;
+	private Vector3 _offset;
+	private Vector3 _armStuckPosition;
 
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
-		var pos = GetMouseWorldPosition();
-		_mouseMarker.GlobalPosition = new Vector3(pos.X, pos.Y, 0);
+
+		if (!_isStuck)
+		{
+			var pos = GetMouseWorldPosition();
+			_mouseMarker.GlobalPosition = new Vector3(pos.X, pos.Y, 0);
+		}
+		else
+		{
+			var pos = GetMouseWorldPosition() - _offset;
+			GlobalPosition = new Vector3(pos.X, pos.Y, 0);;
+			_mouseMarker.GlobalPosition = _armStuckPosition;
+		}
 	}
 
 	private Vector3 GetMouseWorldPosition()
 	{
 		_viewport ??= GetViewport();
 		_camera ??= _viewport.GetCamera3D();
-
 		var pos = _viewport.GetMousePosition();
 		return _camera.ProjectPosition(pos, 0);
 	}
@@ -35,32 +48,31 @@ public partial class Character : MeshInstance3D
 			switch (mouseEvent.ButtonIndex)
 			{
 				case MouseButton.Left:
-					if (mouseEvent.Pressed)
+					if (mouseEvent.Pressed && _canStick)
 					{
-						_leftArmMarker.GlobalPosition = GetMouseWorldPosition();
-						_leftArmMarker.Reparent(_mouseMarker);
-						_rightArmMarker.Reparent(this);
+						var pos = GetMouseWorldPosition();
+						_offset = pos - GlobalPosition;
+						_armStuckPosition = _mouseMarker.GlobalPosition;
+						_isStuck = true;
 					}
 					else if (mouseEvent.IsReleased())
 					{
-						_leftArmMarker.Reparent(this);
-						_rightArmMarker.Reparent(this);
-					}
-					break;
-				case MouseButton.Right:
-					if (mouseEvent.Pressed)
-					{
-						_rightArmMarker.GlobalPosition = GetMouseWorldPosition();
-						_leftArmMarker.Reparent(this);
-						_rightArmMarker.Reparent(_mouseMarker);
-					}
-					else if (mouseEvent.IsReleased())
-					{
-						_leftArmMarker.Reparent(this);
-						_rightArmMarker.Reparent(this);
+						_isStuck = false;
 					}
 					break;
 			}
 		}
+	}
+
+	public void Stick(Node3D node)
+	{
+		GD.Print($"Body entered: {node.Name}");
+		_canStick = true;
+	}
+
+	public void UnStick(Node3D node)
+	{
+		GD.Print($"Body exited: {node.Name}");
+		_canStick = false;
 	}
 }
